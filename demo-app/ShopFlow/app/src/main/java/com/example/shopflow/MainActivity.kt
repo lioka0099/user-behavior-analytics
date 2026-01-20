e0d1921c95426a1094d070ef3b297b36ea8bccb0
lioka0099 <lioka009@gmail.com>
2026-01-20 22:01:37 +0200

    fix bug

--- demo-app/ShopFlow/app/src/main/java/com/example/shopflow/MainActivity.kt
+++ demo-app/ShopFlow/app/src/main/java/com/example/shopflow/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : ComponentActivity() {
         // Initialize the Analytics SDK with your API key and backend URL
         AnalyticsSDK.init(
             context = this,
-            apiKey = "app_c30f3d37",
+            apiKey = BuildConfig.ANALYTICS_API_KEY,
             endpoint = BuildConfig.ANALYTICS_ENDPOINT
         )
         
